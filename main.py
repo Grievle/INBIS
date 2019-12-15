9d7c8fcb10ce0924df74af919d562732a75b5625
labanywhere <cho5095@kookmin.ac.kr>
2019-12-16 01:59:02 +0900

    erase error

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def arg_parse():
     parser.add_argument("--option", dest = 'option', help = "video/webcam/image(default : video)", default = "video", type = str)
     parser.add_argument("--file", dest = 'file', help = 
                         "file log to run detection upon",
-                        defconault = "video.avi", type = str)
+                        default = "video.avi", type = str)
     parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
     parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
     parser.add_argument("--cfg", dest = 'cfgfile', help = 
